b4f4fe69929be30a215c1a3ffcb22d2b9051b420
Deon L <dlundymusic@gmail.com>
2017-03-20 08:27:00 -0400

    First Commit

---
+++ Sales-Person/Form1.Designer.vb
@@ -0,0 +1,220 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PayToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SummaryToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.FontToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ColorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SalesPersonName_Box = New System.Windows.Forms.TextBox()
+        Me.WeeklySales_Box = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Commission_Label = New System.Windows.Forms.Label()
+        Me.Total_Pay_Text = New System.Windows.Forms.Label()
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.FontDialog1 = New System.Windows.Forms.FontDialog()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(359, 24)
+        Me.MenuStrip1.TabIndex = 2
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PayToolStripMenuItem, Me.SummaryToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'PayToolStripMenuItem
+        '
+        Me.PayToolStripMenuItem.Name = "PayToolStripMenuItem"
+        Me.PayToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.PayToolStripMenuItem.Text = "&Pay"
+        '
+        'SummaryToolStripMenuItem
+        '
+        Me.SummaryToolStripMenuItem.Name = "SummaryToolStripMenuItem"
+        Me.SummaryToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.SummaryToolStripMenuItem.Text = "&Summary"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ExitToolStripMenuItem.Text = "E&xit"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ClearToolStripMenuItem, Me.ToolStripSeparator1, Me.FontToolStripMenuItem, Me.ColorToolStripMenuItem})
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(39, 20)
+        Me.EditToolStripMenuItem.Text = "&Edit"
+        '
+        'ClearToolStripMenuItem
+        '
+        Me.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem"
+        Me.ClearToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ClearToolStripMenuItem.Text = "C&lear"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(149, 6)
+        '
+        'FontToolStripMenuItem
+        '
+        Me.FontToolStripMenuItem.Name = "FontToolStripMenuItem"
+        Me.FontToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.FontToolStripMenuItem.Text = "&Font"
+        '
+        'ColorToolStripMenuItem
+        '
+        Me.ColorToolStripMenuItem.Name = "ColorToolStripMenuItem"
+        Me.ColorToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ColorToolStripMenuItem.Text = "&Color"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "&Help"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.AboutToolStripMenuItem.Text = "&About"
+        '
+        'SalesPersonName_Box
+        '
+        Me.SalesPersonName_Box.Location = New System.Drawing.Point(102, 38)
+        Me.SalesPersonName_Box.Name = "SalesPersonName_Box"
+        Me.SalesPersonName_Box.Size = New System.Drawing.Size(100, 20)
+        Me.SalesPersonName_Box.TabIndex = 0
+        '
+        'WeeklySales_Box
+        '
+        Me.WeeklySales_Box.Location = New System.Drawing.Point(102, 64)
+        Me.WeeklySales_Box.Name = "WeeklySales_Box"
+        Me.WeeklySales_Box.Size = New System.Drawing.Size(100, 20)
+        Me.WeeklySales_Box.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(84, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Employee Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(24, 64)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Weekly Sales"
+        '
+        'Commission_Label
+        '
+        Me.Commission_Label.AutoSize = True
+        Me.Commission_Label.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Commission_Label.Location = New System.Drawing.Point(24, 152)
+        Me.Commission_Label.Name = "Commission_Label"
+        Me.Commission_Label.Size = New System.Drawing.Size(79, 15)
+        Me.Commission_Label.TabIndex = 7
+        Me.Commission_Label.Text = "Commission:"
+        Me.Commission_Label.Visible = False
+        '
+        'Total_Pay_Text
+        '
+        Me.Total_Pay_Text.AutoSize = True
+        Me.Total_Pay_Text.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Total_Pay_Text.Location = New System.Drawing.Point(23, 119)
+        Me.Total_Pay_Text.Name = "Total_Pay_Text"
+        Me.Total_Pay_Text.Size = New System.Drawing.Size(92, 24)
+        Me.Total_Pay_Text.TabIndex = 8
+        Me.Total_Pay_Text.Text = "Total Pay:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(359, 210)
+        Me.Controls.Add(Me.Total_Pay_Text)
+        Me.Controls.Add(Me.Commission_Label)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.WeeklySales_Box)
+        Me.Controls.Add(Me.SalesPersonName_Box)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PayToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SummaryToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ColorToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ClearToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents FontToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents SalesPersonName_Box As System.Windows.Forms.TextBox
+    Friend WithEvents WeeklySales_Box As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Commission_Label As System.Windows.Forms.Label
+    Friend WithEvents Total_Pay_Text As System.Windows.Forms.Label
+    Friend WithEvents ColorDialog1 As System.Windows.Forms.ColorDialog
+    Friend WithEvents FontDialog1 As System.Windows.Forms.FontDialog
+
+End Class
---
+++ Sales-Person/Form1.vb
@@ -0,0 +1,98 @@
+﻿Public Class Form1
+
+
+    'Declares Base Pay, Quote, and Commission Constants.
+    Private basePay_CONST = 250, quota_CONST = 1000, c_rate_CONST = 0.15
+    Private WeeklySales_DBL, Commission_DBL, Pay_DBL As Double
+    Private WeeklySales_STR, Commission_STR, Pay_STR As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub PayToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PayToolStripMenuItem.Click
+        ' Calculate and displays the commission and total for sales person.
+
+        Dim EmployeeName = "Unknown"
+        'Dim WeeklySales_DBL, Commission_DBL, Pay_DBL As Double
+        'Dim WeeklySales_STR, Commission_STR, Pay_STR As String
+
+        If WeeklySales_Box.Text = "" Or SalesPersonName_Box.Text = "" Then
+            Dim messageString = "Invalid Input, Try again."
+            MessageBox.Show(messageString, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            WeeklySales_DBL = Double.Parse(WeeklySales_Box.Text)
+            EmployeeName = SalesPersonName_Box.Text
+
+            If WeeklySales_DBL >= quota_CONST Then
+                Commission_DBL = WeeklySales_DBL * c_rate_CONST
+                Pay_DBL = basePay_CONST + Commission_DBL
+                Commission_Label.Visible = True
+            Else
+                Pay_DBL = basePay_CONST
+            End If
+
+            'WeeklySales_STR = WeeklySales_DBL.ToString()
+            Commission_STR = Commission_DBL.ToString()
+            Pay_STR = Pay_DBL.ToString()
+
+            Total_Pay_Text.Text = "Total Pay: " & Pay_STR
+            Commission_Label.Text = "Commission: " & Commission_STR
+
+        End If
+
+        Debug.WriteLine("Employee-Name: " & EmployeeName)
+        Debug.WriteLine("Base Pay: " & basePay_CONST)
+        Debug.WriteLine("Commission-Rate: " & c_rate_CONST)
+        Debug.WriteLine("Commission: " & Commission_DBL)
+        Debug.WriteLine("Pay: " & Pay_DBL)
+        Debug.WriteLine("Quota: " & quota_CONST)
+
+
+    End Sub
+
+    Private Sub SummaryToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SummaryToolStripMenuItem.Click
+        ' Display message-box  that holds total sales, total commissions, and total pay for all salespersons.
+        Dim message1 = "Sales: ", message2 = "Commission: ", message3 = "Pay: "
+        MessageBox.Show(message1 & WeeklySales_DBL & Environment.NewLine &
+                        message2 & Commission_DBL & Environment.NewLine &
+                        message3 & Pay_DBL, "Summary", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub ClearToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ClearToolStripMenuItem.Click
+        ' Clears the name, sales, and pay for the current employee and then resets Focus
+
+        WeeklySales_DBL = 0
+        Commission_DBL = 0
+        Pay_DBL = 0
+
+        WeeklySales_STR = ""
+        Commission_STR = ""
+        Pay_STR = ""
+        Total_Pay_Text.Text = "Total Pay: "
+        SalesPersonName_Box.Text = ""
+        WeeklySales_Box.Text = ""
+        SalesPersonName_Box.Focus()
+        Commission_Label.Visible = False
+
+    End Sub
+
+    Private Sub FontToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FontToolStripMenuItem.Click
+        ' Changes font of Information displayed in total pay text box
+        FontDialog1.ShowDialog()
+        Total_Pay_Text.Font = FontDialog1.Font
+    End Sub
+
+    Private Sub ColorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ColorToolStripMenuItem.Click
+        ' Changes color of Information displayed in total pay box
+        ColorDialog1.ShowDialog()
+        Total_Pay_Text.ForeColor = ColorDialog1.Color
+
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        ' Use message box to display program name and my name.
+        Dim message = "Sales Program | Created By Deon L."
+        MessageBox.Show(message, "About Designer", MessageBoxButtons.OK)
+    End Sub
+End Class
